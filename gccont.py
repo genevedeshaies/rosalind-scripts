8fa2eb73a9ace66b1ee915d066929fbd57a66d0c
genevedeshaies <genevedeshaies@hotmail.com>
2023-01-13 14:25:02 -0500

    Update 1.1

--- gccont.py
+++ gccont.py
@@ -1,22 +1,25 @@
 from Bio import SeqIO
 
-parsedSeq = {}
-gcContCompilation = {}
+def computeGCContent(file, format)->dict:
+    """Parses a sequence file and computes GC content for every sequence."""
+    gcContCompilation = {}
+    with open(file, "r") as fh:
+        for seq_record in SeqIO.parse(file, format):
+            gcCont = (seq_record.seq.count("G") + seq_record.seq.count("C")) / len(seq_record.seq) *100
+            gcContCompilation[seq_record.id] = gcCont
+    return gcContCompilation
 
-highestGC = 0
+def findHighestGC(gcContCompilation):
+    """Looks at the GC contents in a dict and returns the sequence with the highest one"""
+    highestGC = 0
+    for seqid, gc in gcContCompilation.items():
+        if gc > highestGC:
+            highestGC = gc
 
-highestGCSeq = {}
+    for seqid, gc in gcContCompilation.items():    
+        if gc == highestGC:
+            return (seqid, gc)
 
-for seq_record in SeqIO.parse("data.txt", "fasta"):
-    gcCont = (seq_record.seq.count("G") + seq_record.seq.count("C")) / len(seq_record.seq) *100
-    gcContCompilation[seq_record.id] = gcCont
+computedGC = computeGCContent("gc.txt", "fasta")
 
-for seqid, gc in gcContCompilation.items():
-    if gc > highestGC:
-        highestGC = gc
-
-for seqid, gc in gcContCompilation.items():    
-    if gc == highestGC:
-        print(seqid)
-        print(gc)
-    
+print(findHighestGC(computedGC))
